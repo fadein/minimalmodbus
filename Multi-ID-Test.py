cfd348711411d986eb4b429f66259c3935218dcb
Erik Falor (APG) <erikf@apgsensors.com>
2016-08-15 15:51:37 -0600

    add command-line arguments
    -y: specify how many times to cycle
    -t: delay between cycles
    -d: delay between polls of sensors within a cycle

--- Multi-ID-Test.py
+++ Multi-ID-Test.py
@@ -13,6 +13,12 @@ parser.add_argument("-c", "--comport", dest="comport", default="COM8", metavar="
         help="COM port the ModBus network is on")
 parser.add_argument("-n", "--numsensors", dest="numsensors", default=1, metavar="N", type=int,
         help="How many sensors to poll (i.e. maximum sensor address)")
+parser.add_argument('-y', '--cycles', dest='cycles', default=-1, metavar='N', type=int,
+        help='How many times to poll each sensor before terminating')
+parser.add_argument('-t', '--cycle-timeout', dest='cycleTimeout', default=5.0, metavar='N', type=float,
+        help='How long to wait between cycles')
+parser.add_argument('-d', '--cycle-delay', dest='cycleDelay', default=0.3, metavar='N', type=float,
+        help='How long to wait between cycles')
 args = parser.parse_args()
 
 COMPORT  = args.comport
@@ -64,7 +70,7 @@ def echoTime():
 def getModbusValues(dbg, address, ADDR_BASE, stats):
         modbusVal = 0
         register = 300
-        delay = 1
+
         for id in range (1, MAX_ADDR + 1):
                 print  "Polling device ",id
                 print  "-------------------"
@@ -81,31 +87,29 @@ def getModbusValues(dbg, address, ADDR_BASE, stats):
                         print(echoTime())
                         holR=modbusH.read_registers(address - ADDR_BASE, 1)  # last parameter is # of registers.
                         print ("Holding Register -> Slave ID: <%d>  Address : %d  "%(id, address ) + " Value(s) : " + ",".join(str(v) for v in holR ) )
-
-                        time.sleep(1) #wait for a second
+                        time.sleep(args.cycleDelay)
 
                         #Read Input Register
                         print(echoTime())
                         inputR=modbusH.read_registers(register - ADDR_BASE, 1, 4) # last parameter is # of registers.
                         print ("Input Register -> Slave ID: <%d>  Address : %d  "%(id, register ) + " Value(s) : " + ",".join(str(v) for v in inputR ) )
-
                         stats[id]['success'] = stats[id]['success'] + 1
-                        time.sleep(1) #wait for a second
+                        time.sleep(args.cycleDelay)
 
                 except IOError, e:
                         print "%s : %s : %s" % (echoTime(), e , e.errno)
                         stats[id]['failure'] = stats[id]['failure'] + 1
 
                 print
-                time.sleep(cycleDelay)
+                time.sleep(args.cycleDelay)
         return
 
-# modbus function 1 -> Read Coil Status
-# modbus function 2     -> Read Input Status
-# modbus function 3 -> Read Holding Register
-# modbus function 4 -> Read Input Register
-# modbus function 5 -> Force Single Coil
-# modbus function 6 -> Preset Single Register
+# modbus function 1  -> Read Coil Status
+# modbus function 2  -> Read Input Status
+# modbus function 3  -> Read Holding Register
+# modbus function 4  -> Read Input Register
+# modbus function 5  -> Force Single Coil
+# modbus function 6  -> Preset Single Register
 # modbus function 15 -> Force Multiple Coils
 # modbus function 16 -> Preset Multiple Registers
 
@@ -113,16 +117,16 @@ def getModbusValues(dbg, address, ADDR_BASE, stats):
 utc = UTC()
 try:
         statistics = Statistics(MAX_ADDR)
-        cycles = 1
-        while True:
+        cycle = 0
+        while args.cycles - cycle != 0:
+                cycle = cycle + 1
                 print "=============================="
-                print "Cycle #%d" % cycles
+                print "Cycle #%d" % cycle
                 print "=============================="
                 getModbusValues(debug, add, ADDR_BASE, statistics)
                 print
                 print
-                cycles = cycles + 1
-                time.sleep(5)
+                time.sleep(args.cycleTimeout)
 
 except KeyboardInterrupt:
         print
