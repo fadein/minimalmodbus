3184c6628ab5d0cf2db4e1a80f3ec9c4baba6924
Erik Falor (APG) <erikf@apgsensors.com>
2016-08-16 11:55:04 -0600

    wip: new CVS logging framework

--- cvstest.py
+++ cvstest.py
@@ -1,16 +1,32 @@
-import csv, sys
+import csv, sys, datetime
+
+# can I have a nested class in Python?
+class UTC(datetime.tzinfo):
+    ZERO = datetime.timedelta(0)
+    def utcoffset(self, dt):
+        return UTC.ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        return UTC.ZERO
+
 
 class Statistics():
-    def __init__(self, maxdev, fileObj = sys.stdout):
-        self.statistics = {}
+    def __init__(self, fileObj = sys.stdout):
+        self.eventLog = []
         self.file = fileObj
+        self.total = 0
+        self.success = 0
+        self.failure = 0
+        self.utc = UTC()
 
-        for i in range (1, maxdev + 1):
-            self.statistics[i] = {'id': i, 'total': 0, 'success': 0, 'failure': 0}
 
     def getiter(self):
-        i = iter(sorted(self.statistics.iteritems()))
+        i = iter(sorted(self.eventLog))
         return i
+
 
     def __repr__(self):
         string = ''
@@ -18,47 +34,48 @@ class Statistics():
             string = string + str(j) + "\n"
         return string
 
+
     def __getitem__(self, i):
-        return self.statistics[i]
+        return self.eventLog[i]
 
-    def failure(self, i):
-        self.statistics[i]['total'] = self.statistics[i]['total'] + 1
-        self.statistics[i]['failure'] = self.statistics[i]['failure'] + 1
+    def logReading(self, device, register, value, desc):
+        record = {'when': datetime.datetime.now(self.utc).strftime("%a %b %d %H:%M:%S %Z %Y"),
+                'device': device,
+                'register': register,
+                'value': value,
+                'desc': desc}
+        self.eventLog.append(record)
+        self.total += 1
 
-    def success(self, i):
-        self.statistics[i]['total'] = self.statistics[i]['total'] + 1
-        self.statistics[i]['success'] = self.statistics[i]['success'] + 1
-    
+    def badReading(self, device, register, desc=''):
+        self.logReading(device, register, False, desc)
+        self.failure += 1
+
+    def goodReading(self, device, register, value, desc=''):
+        self.logReading(device, register, value, desc)
+        self.success += 1
+
+    def descLine(self, desc):
+        record = {'when'   : '',
+                'device'   : '',
+                'register' : '',
+                'value'    : '',
+                'desc'     : desc}
+        self.eventLog.append(record)
+
     def writeCSV(self):
-        w = csv.DictWriter(self.file, ['id', 'total', 'success', 'failure'])
+        w = csv.DictWriter(self.file, [ 'when', 'device', 'register', 'value', 'desc', ])
         w.writeheader()
-        for j in self.getiter():
-            w.writerow(j[1])
+        for r in self.eventLog:
+            w.writerow(r)
 
+    def summarize(self):
+        self.writeCSV()
+        print self
 
-with open('derp.cvs', 'w') as cvsfile:
-
-    s = Statistics(5, fileObj = cvsfile)
-    for i in range(8):
-        s.success(1)
-
-    for i in range(4):
-        s.success(2)
-        s.failure(2)
-
-    for i in range(2):
-        s.success(3)
-        s.failure(3)
-        s.failure(3)
-        s.failure(3)
-
-    for i in range(3):
-        s.failure(4)
-        s.success(4)
-        s.success(4)
-        s.success(4)
-
-    for i in range(8):
-        s.failure(5)
-
-    s.writeCSV()
+s = Statistics()
+s.descLine('just trying to make it in this wirld')
+s.goodReading(1, 400, 1492)
+s.goodReading(2, 400, 1492)
+s.goodReading(3, 400, 1492)
+s.writeCSV()
